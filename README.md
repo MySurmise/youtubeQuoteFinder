8ac5eec47fa07053b522d3beb7566be7f7bd4656
MySurmise <marlbos@outlook.de>
2021-06-01 03:53:15 +0200

    first thing downloader and half of cutter

--- README.md
+++
@@ -1,2 +0,0 @@
-# youtubeQuoteFinder
-Bot that downloads subtitles from Youtube and finds quotes and someday will be able to extract the video from it.
---
+++ downloader.py
@@ -0,0 +1,55 @@
+def ytdownload(url):
+    from pprint import pprint
+    import requests
+    import youtube_dl
+    import os
+    import json
+
+
+    extension = "mp4"
+
+    os.system('pip install -U youtube-dl --user')
+    
+    
+    ydl = youtube_dl.YoutubeDL({'outtmpl': 'downloads/{0} - %(title)s/%(title)s.%(ext)s'.format(len(os.listdir('downloads'))), 'forcetitle':'true', 'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4', 'writesubtitles':True, 'writeautomaticsub': True})
+
+    with ydl:
+        result = ydl.download([url])
+
+
+
+
+'''
+For custom download with extract info 
+
+#print(video)
+#print(json.dumps(video, indent=4))
+#print(json.dumps(video["formats"], indent=4))
+resolutions = {}
+resolutionslist = []
+
+#format ids: 
+
+for format in video["formats"]:
+    if format["ext"] == extension:
+        try:
+            resolutions[format['quality']][format['format_id']] = {'vcodec': format['vcodec'], 'url': format['url']} #if yes, append the format id, format name and url in a dict to it.
+        except:
+            resolutions[format['quality']] = {}
+            resolutions[format['quality']][format['format_id']] = {
+                                                    "vcodec": format['vcodec'], 
+                                                    "url": format['url']
+                } 
+#if yes, append the format id, format name and url in a dict to it.
+
+        resolutionslist.append(str(format['quality']) + ' = ' + format['url'])
+
+
+#print(json.dumps(resolutions, indent=4))
+highest_quality = resolutions[max(resolutions.keys())]
+highest_formatid = highest_quality[max(highest_quality)]
+print(highest_formatid['url'])
+
+#pprint(resolutionslist)
+
+'''
